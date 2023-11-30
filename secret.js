fa56d74fb240fed15fd73e582557e9a555a15db9
LinkChange <1045497252@qq.com>
2023-12-01 06:07:13 +0800

    Update secret.js

--- secret.js
+++ secret.js
@@ -1,7 +1,7 @@
 module.exports = {
   wechat: {
-    appid: "wx86aee56017aeec46",
-    appsecret: "d7dbc969ddc4a734cc1a4ae44430c0ab",
+    appid: "test",
+    appsecret: "test",
     mchid: "test",
     mcnsecret: "test",
     kfappid: "test",
@@ -11,30 +11,30 @@ module.exports = {
     clientIp: "47.114.4.233",
     showName: "虎锐科技",
     wishing: "《创业时代2.0》红包提现",
-    mch_id: "1601743953",
-    wxkey: "QwEr1TyUi2IoP3ZxC4VbNm5AsDfGhJkL", //key为在微信商户平台(pay.weixin.qq.com)-->账户设置-->API安全-->密钥设置
-    appid: "wx86aee56017aeec46", //微信支付app的appid
+    mch_id: "test",
+    wxkey: "test", //key为在微信商户平台(pay.weixin.qq.com)-->账户设置-->API安全-->密钥设置
+    appid: "test", //微信支付app的appid
     // wxappid: 'wxfa6956dbc4c0eb4a', //微信公众号appid
-    appsecret: "d7dbc969ddc4a734cc1a4ae44430c0ab", //公众号secret
+    appsecret: "test", //公众号secret
   },
   sms: {
-    accessKeyId: "LTAI4GK4CGGPEG7jQ7gsQL8o",
-    accessKeySecret: "zjvb3is3hKqk1LDOu8z5JGxse6VOVY",
+    accessKeyId: "test",
+    accessKeySecret: "test",
     endpoint: "https://dysmsapi.aliyuncs.com",
     apiVersion: "2017-05-25",
     SignName: "虎锐",
-    TemplateCode: "SMS_209162952",
+    TemplateCode: "test",
     TemplateParam: JSON.stringify({ game: "创业时代2.0" }),
-    PhoneNumbers: "15920075096,17520330821,13724389777,18565430412", // 家伟，林成，肖总，培宇
+    PhoneNumbers: "17520330821", // 家伟，林成，肖总，培宇
   },
   smsMaxMoneyToday: {
-    accessKeyId: "LTAI4GK4CGGPEG7jQ7gsQL8o",
-    accessKeySecret: "zjvb3is3hKqk1LDOu8z5JGxse6VOVY",
+    accessKeyId: "test",
+    accessKeySecret: "test",
     endpoint: "https://dysmsapi.aliyuncs.com",
     apiVersion: "2017-05-25",
     SignName: "虎锐",
-    TemplateCode: "SMS_211486089",
+    TemplateCode: "test",
     TemplateParam: JSON.stringify({ game: "创业时代2.0" }),
-    PhoneNumbers: "15920075096,17520330821,13724389777,18565430412", // 家伟，林成，肖总，培宇
+    PhoneNumbers: "17520330821", // 家伟，林成，肖总，培宇
   },
 };
